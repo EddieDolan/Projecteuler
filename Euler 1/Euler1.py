0c5160415a114103a372d5a4c5ed36c1fde6332e
EddieDolan <mabelkittydurden@gmail.com>
2014-06-16 18:49:58 -0500

    initial commit

---
+++ Euler 1/Euler1.py
@@ -0,0 +1,16 @@
+__author__ = 'eddie'
+
+
+def euler1(num_max):
+    temp_list =[]
+    for x in range(100):
+        if x%5==0:
+            temp_list.append(x)
+        elif x%3==0:
+            temp_list.append(x)
+    return sum(temp_list)
+
+print euler1(100)
+
+
+print sum([x for x in range(100) if x%3==0 or x%5==0])
