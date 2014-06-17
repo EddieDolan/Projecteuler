f8e12975a3318beac4a73529f5fa287582a40903
EddieDolan <mabelkittydurden@gmail.com>
2014-06-16 19:30:37 -0500

    updating E1

--- Euler 1/Euler1.py
+++ Euler 1/Euler1.py
@@ -14,3 +14,15 @@ print euler1(100)
 
 
 print sum([x for x in range(100) if x%3==0 or x%5==0])
+
+b = []
+for x in range(10):
+    if x%5==0:
+        b.append(x*2)
+print b
+
+a =  [x*2 for x in range(10)]
+print a
+
+c = [x for x in range(10) if x%5==0]
+print c
